6e8f469f17f01c8c4c1033b5b1aa259a7e443224
TranAnhPhuong0797 <67380535+TranAnhPhuong0797@users.noreply.github.com>
2024-03-24 16:01:55 +0700

    commit

---
+++ Test Suites/CI CD Test Collection Suilt.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>CI CD Test Collection Suilt</name>
+   <tag></tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>QAenv</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/CI CD Test suilt</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>PREPRODenv</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Firefox</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/CI CD Test suilt</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
--- Test Suites/CI CD Test suilt.ts
+++ Test Suites/CI CD Test suilt.ts
@@ -19,19 +19,6 @@
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>8312a2a6-8e93-447c-9572-9f6d2a4615a2</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/CI CD Integration/CICD_Write test case by script</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>53f1dbeb-6900-420b-bbb6-f7d0693a80e9</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
       <guid>c1ea5819-a7b5-49f1-9c43-370a0c9bef7c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
